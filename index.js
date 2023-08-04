d56106f37392811c2482c7d9e308248d2f33ead5
yolando20 <yolandomanullang@student.telkomuniversity.ac.id>
2023-08-05 01:54:57 +0700

    revisi 1

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 const twilio = require('twilio');
 const notifier = require('node-notifier');
+const player = require('play-sound')();
+
+
 
 // Konfigurasi akun Twilio
 const accountSid = 'AC14b5ddcb5f1f2dcddb775c5f050ccd8a';
-const authToken = '28c43f283814d7a0dd6ddf6123efde4f';
+const authToken = 'b0922bad7d52fe3051fc375465cced53';
 const twilioNumber = '+14155238886'; // Nomor WhatsApp yang sudah terverifikasi di Twilio
 const targetWhatsAppNumber = '+6282267852621'; // Nomor WhatsApp tujuan
 
 const client = twilio(accountSid, authToken);
 
-const websiteUrl = 'https://pandang.istanapresiden.go.id/';
+const websiteUrl = 'https://pointblank.id/';
+const soundFile = './music.mp3'; // Ganti dengan path ke file audio Anda
+
+const MAX_NOTIFICATION_COUNT = 10;
+const MAX_WHATSAPP_COUNT = 10;
+const STATUS_OK = 200;
+
+let hasPlayedSound = false;
+let notificationCount = 0;
+let whatsappCount = 0;
 
 function checkWebsiteStatus() {
   axios.get(websiteUrl)
     .then((response) => {
-      if (response.status === 200) {
-        const $ = cheerio.load(response.data);
-        const targetText = "INI MERUPAKAN VERSI UNTUK UJI COBA SEBELUM LAUNCHING";
-        if ($('#targetElementId').text().includes(targetText)) {
-          console.log("Belum bisa daftar. Informasi: INI MERUPAKAN VERSI UNTUK UJI COBA SEBELUM LAUNCHING");
-        } else {
-          console.log(`Website ${websiteUrl} berhasil dimuat (Status Code: 200)`);
+      if (response.status === STATUS_OK) {
+        interval = 3000;
+        if (!hasPlayedSound) {
+          console.log(`Website ${websiteUrl} berhasil dimuat (Status Code: ${STATUS_OK})`);
+          playSuccessSound();
+          hasPlayedSound = true;
+        }
+
+        // Menampilkan notifikasi desktop
+        if (notificationCount < MAX_NOTIFICATION_COUNT) {
+          console.log(`Website ${websiteUrl} berhasil dimuat (Status Code: ${STATUS_OK})`);
           showDesktopNotification('Website berhasil dimuat');
+          notificationCount++;
+        }
+
+        // Mengirim pesan WhatsApp
+        if (whatsappCount < MAX_WHATSAPP_COUNT) {
           sendWhatsAppMessage('Website berhasil dimuat');
+          whatsappCount++;
+        }
+
+        if(whatsappCount == MAX_WHATSAPP_COUNT && notificationCount == MAX_NOTIFICATION_COUNT){
+            console.log(`Website ${websiteUrl} berhasil dimuat (Status Code: ${STATUS_OK})`);
         }
       } else {
         console.log(`Gagal Broo =>  (Status Code: ${response.status})`);
@@ -35,12 +60,19 @@ function checkWebsiteStatus() {
     });
 }
 
+function playSuccessSound() {
+  player.play(soundFile, (err) => {
+    if (err) {
+      console.error(`Gagal memutar suara: ${err}`);
+    }
+  });
+}
+
 function showDesktopNotification(message) {
   notifier.notify({
     title: 'Website Status',
     message: message,
     sound: true,
-    wait: true,
   });
 }
 
@@ -55,5 +87,5 @@ function sendWhatsAppMessage(message) {
 }
 
 // Mulai memeriksa setiap 1 detik
-const interval = 2000; // 1 detik dalam milisekon
+interval = 1000; // 1 detik dalam milisekon
 setInterval(checkWebsiteStatus, interval);
